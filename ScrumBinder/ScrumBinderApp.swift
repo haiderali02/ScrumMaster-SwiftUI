4c87725ad155e48af35dfac67bdf1cc7b51f212d
personal <inf.haiderali@gmail.com>
2022-10-31 12:53:25 +0500

    Initial Commit

---
+++ ScrumBinder/ScrumBinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumBinderApp.swift
+//  ScrumBinder
+//
+//  Created by Haider Ali on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumBinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
